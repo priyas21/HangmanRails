e5e8b20c9ad751cb609174d0da8d990be780d243
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-06-28 10:25:20 +1200

    Update create and new action
    Previously we were not checking what happens when @game.save is false
    Now when the form doesn't save means there is some invalid input then it
    will reload the form.
    We use render instead of redirect_to as render will send the instance of
    game to the new template when it's rendered while redirect_to will
    refresh the page again.
    
    New action will create a new instance of the game.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,18 +3,22 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
   end
+
   def show
     @game = Game.find(params[:id])
   end
 
   def new 
-    
+    @game = Game.new
   end
   
   def create
     @game = Game.new(game_params)
-    @game.save
-    redirect_to @game
+    if @game.save
+      redirect_to @game
+    else
+      render 'new'
+    end
   end
 
   private
