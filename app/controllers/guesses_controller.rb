2dfca57130a2a3bebf0aac1ce30a599d298f18bb
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-06-29 14:41:53 +1200

    Add create action in guesses controller and view for displaying the guesses
    Create action will keep track of game_id with which the guess is related
    We will use the create method which comes with association @game.guesses.create
    to create and save the guesses
    
    After submitting the guess it will redirect back to show action

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,2 +1,11 @@
 class GuessesController < ApplicationController
+  def create
+    @game = Game.find(params[:game_id])
+    @guess = @game.guesses.create(guess_params)
+    redirect_to game_path(@game)
+  end
+  private
+  def guess_params
+    params.require(:guess).permit(:guessed_letter)
+  end
 end
