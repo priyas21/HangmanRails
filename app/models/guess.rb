1a43682cf26cb6f63f21a6c84f98cdf351a44fb4
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-03 10:04:22 +1200

    Add the validation for guess made
    The validation will check whether user have enter only one letter
    It will match with the regular expression.

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,4 +1,6 @@
 class Guess < ApplicationRecord
   belongs_to :game
-  # validates :guessed_letter, presence: true
+  validates :guessed_letter, presence: true,
+            format: { with: /\A[A-Za-z]\z/,
+            message: "only allows letters" }
 end
