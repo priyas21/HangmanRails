b3ea146a5e534c0be16d64cf3403f06bf2d7295a
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-03 11:22:58 +1200

    Generate new migration to add number_of_lives column
    The games table had name column so in order to add new column
    generate new migration and add integer type column in it

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_27_232157) do
+ActiveRecord::Schema.define(version: 2018_07_02_231510) do
 
   create_table "games", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "number_of_lives"
   end
 
   create_table "guesses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
