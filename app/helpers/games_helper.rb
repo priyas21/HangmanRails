2794439b519b3cecf382e57eb0805fe7013562ad
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-09 15:13:17 +1200

    Use helpers to extract the logic out of the view
    GamesHelper class have two  methods:
    - show_clue: It will take method as an argument which is to build the clue
    and will format it to print the dashes
    - game_progress: It will check whether the game is in one of the
    three states , game_in_progress, won or lost
    
    In the _form partial called the helper methods

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,18 @@
 module GamesHelper
+   def show_clue(clue)
+    formatted_clue = clue.map do |c|
+      c.nil? ? '_' : c
+    end
+    formatted_clue.join(" ")
+  end
+
+  def game_progress(game)
+    if game.game_in_progress?
+      "You have #{game.remaining_lives.to_s} lives remaining"
+    elsif game.won?
+      "You have won the game"
+    else
+      "You are hanged"
+    end
+  end
 end
