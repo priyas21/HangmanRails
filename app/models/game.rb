93f2d435fa0c88cf48da8f7a1370c9144a380100
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-09 15:00:12 +1200

    Calculate the incremental states of the game
    Game will calculate at each submission of guess:
    - remaining_lives
    - It will build the clue
    
    Game will add the letter guessed to the guesses
    .compact will remove the nil when there is no guess and will
    return an empty array
    
    Game will check three states whether the game is
    - game_in_progress
    - won
    - lost
    (The above three states are dependent on word_guessed and number of lives)
    
    Few private methods are created
    - to calcuate the incorrect guesses
    - to calculate unguessed characters

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,48 @@
 class Game < ApplicationRecord
-  
   has_many :guesses, dependent: :destroy
   validates :name, presence: true
+
+  def guessed_letters
+   guesses.pluck(:letter).compact
+  end
+
+  def clue
+    build_clue(word, guessed_letters)
+  end
+
+  def remaining_lives
+    initial_number_of_lives - incorrect_guesses.length
+  end
+
+  def game_in_progress?
+    !won? && !lost?
+  end
+
+  def won?
+    word_guessed? && remaining_lives >= 1
+  end
+
+  def lost?
+    !word_guessed? && remaining_lives < 1
+  end
+
+  def word_guessed?
+    unguessed_characters.empty?
+  end
+
+  private
+
+  def incorrect_guesses
+    guessed_letters - word.downcase.chars.uniq
+  end
+
+  def build_clue(word, guesses)
+    word.chars.map do |letter|
+      guesses.include?(letter.downcase) ? letter : nil
+    end
+  end
+
+  def unguessed_characters
+    word.downcase.chars.uniq - guessed_letters
+  end
 end
