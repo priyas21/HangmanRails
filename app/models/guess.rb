0f448bd5c62910a98aaa20f442f1c4886af35d6e
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-09 14:56:38 +1200

    Update the regular expression
    Remove + as it was allowing multiple characters

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,8 +1,6 @@
 class Guess < ApplicationRecord
   belongs_to :game
-  validates :guessed_letter, presence: true,
-            format: { with: /\A[a-zA-Z]+\z/ },
+  validates :letter, presence: true,
+            format: { with: /\A[a-zA-Z]\z/ },
             uniqueness: { scope: :game_id }
-    
-  
 end
