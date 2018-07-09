559984bf722a8701cd6832d8693fe28c1c63413e
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-09 12:28:27 +1200

    Modify the code to split new line at the end of the word
    Previously the words were saved in an array. But at the end of each
    word there was \n appended.
    
    Now the \n is removed using scrub method

--- app/services/generate_random_word.rb
+++ app/services/generate_random_word.rb
@@ -1,13 +1,6 @@
 class GenerateRandomWord
-
-  def words
-    line_num=0
-    word = []
-    text=File.open('app/assets/words.txt').read
-    text.each_line do |line|
-      word[line_num] = line
-      line_num += 1
-    end
-    word.sample
+  def call
+    word=File.open('app/assets/words.txt').read
+    word.scrub.split.sample
   end
 end
