a029295821de67f6c2f3ead77fdea3b2f4a9fab5
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-10 16:34:12 +1200

    Use have_many matcher in game spec
    have_many is used to test the association between game and guesses
    which is one to many; one game has many guesses

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,7 @@
 require "rails_helper"
 
 RSpec.describe Game, :type => :model do
- it { should validate_presence_of(:name) }
+  it { should have_many(:guesses) }
+  
+  it { should validate_presence_of(:name) }
 end
