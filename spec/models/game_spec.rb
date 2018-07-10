cec1f1b95513a66ff20c01493fcd27d123cb3f5a
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-10 16:25:51 +1200

    Use shoulda matcher to validate the presence of name

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,13 +1,5 @@
 require "rails_helper"
 
 RSpec.describe Game, :type => :model do
-  it "is not created without a name" do
-    game = Game.new(name: nil)
-    expect(game).to_not be_valid
-  end
-
-  it "is created with a name" do
-    game = Game.new(name: "Lilly")
-    expect(game).to be_valid
-  end
+ it { should validate_presence_of(:name) }
 end
