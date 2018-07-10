4b7a59ef23d2610778fa3bb68074423603056766
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-10 16:36:25 +1200

    Use belong_to matcher
    belong_to is used to test the belongs_to association
    between game and guesses model
    It means that particluar guess belongs to only one game_id

--- spec/models/guess_spec.rb
+++ spec/models/guess_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.describe Guess, :type => :model do
+  it {should belong_to(:game) }
+  
   it { should validate_presence_of(:letter) }
 
   it { should validate_uniqueness_of(:letter).scoped_to(:game_id) }
