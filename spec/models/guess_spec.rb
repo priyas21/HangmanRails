e38e82f423607a23112e41fd67d3b0d0aa0bb4e7
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-10 15:49:20 +1200

    Use shoulda-matchers to test the validation of guesses
    Previously use rspec-rails to test the validation
    
    Now use matchers of shoulda gem
    
    Validate the presence of letter's presence

--- spec/models/guess_spec.rb
+++ spec/models/guess_spec.rb
@@ -1,25 +1,5 @@
 require "rails_helper"
 
 RSpec.describe Guess, :type => :model do
-  game = Game.new(name: "piu")
-  subject(:guess) { game.guesses.new }
-  
-  context "with empty input" do
-    it "is not valid" do
-      guess.letter = nil
-      expect(guess).to_not be_valid
-    end
-  end
-
-  context "with input as number, special characters or multiple characters" do
-    it "is not valid" do
-      guess.letter = '@'
-      expect(guess).to_not be_valid
-    end
-  end
-
-  it "is valid with valid input" do
-    guess.letter = "p"
-    expect(guess).to be_valid
-  end
+  it { should validate_presence_of(:letter) }
 end
