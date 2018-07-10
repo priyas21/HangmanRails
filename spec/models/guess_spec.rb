d76afa24815cf374fb8f1efe908061abf22e8376
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-10 16:27:10 +1200

    Validate the uniqueness of letter
    Use shoulda macther to test the duplication of letter

--- spec/models/guess_spec.rb
+++ spec/models/guess_spec.rb
@@ -2,4 +2,8 @@ require "rails_helper"
 
 RSpec.describe Guess, :type => :model do
   it { should validate_presence_of(:letter) }
+
+  it { should validate_uniqueness_of(:letter).scoped_to(:game_id) }
+
+  # it { should validate_format_of(:letter).with(/\A[a-zA-Z]\z/) }
 end
