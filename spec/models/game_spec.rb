08a0e735f540d9c2fd6dfd62b0315fe561beb817
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-11 15:26:01 +1200

    Setting up the Game
    Initialized the name and the subject as new instance of Game

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,10 +1,15 @@
 require "rails_helper"
 
 RSpec.describe Game, :type => :model do
+  let(:name) { 'Lilly' }
+
+  subject(:game) { Game.new(:name => name,
+          :initial_number_of_lives => 8, :word => 'Rails' )}
+
   describe "Associations" do
     it { should have_many(:guesses) }
   end
-  
+
   describe "Validations" do
     it { should validate_presence_of(:name) }
   end
