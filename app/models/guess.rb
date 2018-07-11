b91b8d74573c55cfc4024b31f73b73fa57737c1c
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-11 17:08:43 +1200

    Downcased the input by player before validating it
    The user is allowed to input either uppercase or lowercase , but before
    validating its lowercased permanently

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,6 +1,11 @@
 class Guess < ApplicationRecord
   belongs_to :game
+  before_validation :downcase_letter
   validates :letter, presence: true,
             format: { with: /\A[a-zA-Z]\z/ },
             uniqueness: { scope: :game_id }
+
+  def downcase_letter
+    self.letter.downcase!
+  end
 end
