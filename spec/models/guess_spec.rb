736df3531467f9a05451cf018fe4f5c89bac77bb
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-11 15:19:00 +1200

    Validate the letter input by user using shoulda matchers and use is_expected_to
     Previously used should in the examples which was validating on the object itself
    
     Now using is_expected_to as it will validate on a wrapped object
    
     Validating the letter's presence, uniqueness and against invalid input

--- spec/models/guess_spec.rb
+++ spec/models/guess_spec.rb
@@ -2,12 +2,16 @@ require "rails_helper"
 
 RSpec.describe Guess, :type => :model do
   describe "Associations" do
-    it {should belong_to(:game) }
+    it {is_expected.to belong_to(:game) }
   end
 
   describe "Validations" do
-    it { should validate_presence_of(:letter) }
+    it { is_expected.to validate_presence_of(:letter) }
 
-    it { should validate_uniqueness_of(:letter).scoped_to(:game_id) }
+    it { is_expected.to validate_uniqueness_of(:letter).scoped_to(:game_id) }
+
+    it { is_expected.to allow_value('a').for(:letter) }
+
+    it { is_expected.to_not allow_values(1, '@', 'jhgjh').for(:letter) }
   end
 end
