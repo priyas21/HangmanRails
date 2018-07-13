8bd7ccd8c93136a38d1edbb4c5f5a1dbd23c819a
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-13 15:57:02 +1200

    Test the index action of game controller
    Use render_template matcher to specify that a request
    renders a given template

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -8,4 +8,17 @@ RSpec.describe GamesController, :type => :controller do
   it { is_expected.to route(:get, '/games/new').to(action: :new) }
 
   it { is_expected.to route(:get, 'games/60').to(action: :show, id: 60)}
+
+  describe "GET index" do
+    it "assigns @game" do
+      game = Game.create!(:name => 'ppp')
+      get :index
+      expect(assigns(:games)).to eq([game])
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
 end
