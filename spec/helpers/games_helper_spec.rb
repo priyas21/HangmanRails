37b7588e3d7d861462155332d7ba7022a3901bca
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-17 15:30:04 +1200

    Create test for GamesHelper class
    - Test the show clue method
    - Test the game_progress method
      - game_progress method will stub remaining_lives, in_progress?,
        won? methods as we are not testing them.

---
+++ spec/helpers/games_helper_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe GamesHelper, :type => :helper do
+  describe "#show_clue" do
+    let(:clue) { [nil, 'a', nil, nil, 't'] }
+
+    it "returns the fomatted clue" do
+      expect(helper.show_clue(clue)).to eq "_ a _ _ t"
+    end
+  end
+
+  describe "#game_progress" do
+    let(:game) { Game.create!(:name => "lilly", :word => "Rails") }
+
+    context "when game is in progress" do
+      it "returns number of lives remaining" do
+        allow(game).to receive(:remaining_lives).and_return(5)
+        allow(game).to receive(:in_progress?).and_return(true)
+        expect(helper.game_progress(game)).to eq "You have 5 lives remaining"
+      end
+    end
+
+    context "when game is won" do
+      it "displays the won message" do
+        allow(game).to receive(:in_progress?).and_return(false)
+        allow(game).to receive(:won?).and_return(true)
+        expect(helper.game_progress(game)).to eq "You have won the game"
+      end
+    end
+
+
+    context "when game is lost" do
+      it "displays the lost message" do
+        allow(game).to receive(:in_progress?).and_return(false)
+        allow(game).to receive(:won?).and_return(false)
+        expect(helper.game_progress(game)).to eq "You are hanged"
+      end
+    end
+  end
+end
