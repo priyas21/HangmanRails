b394276fe7517eecea3ecb1f3404e3dae99e0f4c
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-19 09:49:17 +1200

    Add the steps defination of Play game feature
    - Given user is on game page
      - This step defination will set up the game by creating
       the game
    - When and Then are ambigious step definations as they are
    redundant for two scenarios, so these definations are in
    shared_steps file

--- features/step_definitions/play_game_steps.rb
+++ features/step_definitions/play_game_steps.rb
@@ -1,1 +1,5 @@
-
+Given("User is on game page") do
+  visit new_game_path
+  fill_in("Player", :with => "Priya")
+  click_button('Create Game')
+end
--- features/step_definitions/shared_steps.rb
+++ features/step_definitions/shared_steps.rb
@@ -1,3 +1,16 @@
+When("User enters the {string} with {string}") do |field, value|
+  fill_in(field, :with => value)
+  click_button('Submit')
+end
+
+When("User clicks the {string} link") do |link|
+  click_link(link)
+end
+
 Then("User should see {string}") do |content|
+  expect(page).to have_content(content)
+end
+
+Then("User gets an alert {string}") do |content|
   expect(page).to have_content(content)
 end
