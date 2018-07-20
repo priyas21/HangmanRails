5d0d8aeb8e837d14262e5111395ca6dc9732f745
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-07-20 14:53:52 +1200

    Add validation to check the length of letter

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,6 +1,7 @@
 class Guess < ApplicationRecord
   belongs_to :game
   validates :letter, presence: true,
+            length: {minimum: 1, maximum: 1},
             format: { with: /\A[a-zA-Z]\z/ },
             uniqueness: { scope: :game_id }
 
